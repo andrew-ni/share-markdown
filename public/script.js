32dd0986828ef13d4edae75186d3afed553cb787
andrewni <ajni@ucdavis.edu>
2017-10-31 17:13:12 -0700

    Added check for other people's textbox change

--- public/script.js
+++ public/script.js
@@ -7,11 +7,30 @@ window.onload = function() {
 
     var convertTextAreaToMarkdown = function() {
         var markdownText = pad.value;
+        previousMarkdownValue = markdownText;
         html = converter.makeHtml(markdownText);
         markdownArea.innerHTML = html;
     };
 
+    var didChangeOccur = function() {
+        if(previousMarkdownValue != pad.value) {
+            return true;
+        }
+        return false;
+    };
+
+    setInterval(function(){
+        if(didChangeOccur()){
+            convertTextAreaToMarkdown();
+        }
+    }, 1000);
+
     pad.addEventListener('input', convertTextAreaToMarkdown);
 
-    convertTextAreaToMarkdown();
+    // open a sharejs connection to 'home' then attach the textarea to the object returned by this connection
+    // keeps our textarea in sync with everyone else's textarea
+    // TODO: A's markdownarea won't be updated when B makes a change
+    sharejs.open('home', 'text', function(error, doc) {
+        doc.attach_textarea(pad);
+    })
 }
