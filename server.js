1d7248a844b1f7518c20c61a5bee546ccecb5a2a
andrewni <ajni@ucdavis.edu>
2017-10-31 22:39:51 -0700

    updated server.js

--- server.js
+++ server.js
@@ -15,24 +15,29 @@ app.get('/', function(req, res) {
 });
 app.get('/(:id)', function(req, res) {
     res.render('pad');
-})
+});
 
 //get sharejs dependencies
 var sharejs = require('share');
-require('redis');
 
+// set up redis server
+var redisClient;
+console.log(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+    var rtg = require("url").parse(process.env.REDISTOGO_URL);
+    redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+    redisClient.auth(rtg.auth.split(":")[1]);
+} else {
+    redisClient = require("redis").createClient();
+}
 //options for sharejs
 var options = {
-    db: {type: 'redis'},
+    db: {type: 'redis', client: redisClient},
 };
 
 // attach the express server to sharejs
 sharejs.server.attach(app.options);
 
 // listen on port 8000 or the port defined for heroku
-var port = process.env.PORT || 8000;
-app.listen(port);
-
-
 var port = process.env.PORT || 8000;
 app.listen(port);
